2f98da17c56540369ddc6f7f50d31c7dfbbee305
A <woonhakazira@gmail.com>
2025-12-16 10:19:21 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+const slides = document.querySelectorAll('.slide');
+let current = 0;
+
+document.getElementById('next').onclick = () => {
+  slides[current].classList.remove('active');
+  current = (current + 1) % slides.length;
+  slides[current].classList.add('active');
+};
+
+document.getElementById('prev').onclick = () => {
+  slides[current].classList.remove('active');
+  current = (current - 1 + slides.length) % slides.length;
+  slides[current].classList.add('active');
+};
